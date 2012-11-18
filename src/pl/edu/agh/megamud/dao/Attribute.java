37f998605486d7beb380fe366c0dd8ab2a97a94f
Lukasz Kieda <extralucas@gmail.com>
2012-11-18 15:10:02 +0100

    Added initialization of the strength attribute.

--- src/pl/edu/agh/megamud/dao/Attribute.java
+++ src/pl/edu/agh/megamud/dao/Attribute.java
@@ -7,4 +7,5 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "attribute")
 public class Attribute extends AttributeBase{
 	
+	public static final String STRENGTH = "strength";
 }

4b3c69b9d4af0b2d61bbaffb8925c85e92f582db
Devuatl <kieda.lukasz@gmail.com>
2012-11-20 08:33:27 +0100

    Added eastern part of the cave.

--- src/pl/edu/agh/megamud/world/CaveInitializer.java
+++ src/pl/edu/agh/megamud/world/CaveInitializer.java
@@ -31,46 +31,33 @@ public class CaveInitializer {
 		return loc1;
 	}
 	
-	private static void preparePortal(String from,String to,String name) throws SQLException{
-		Location loc1 = LocationBase.createDao()
-				.queryBuilder()
-					.where().eq("name", from)
-					.and().eq("module", getId())
-				.query().
-				get(0);
-		Location loc2 = LocationBase.createDao()
-				.queryBuilder()
-					.where().eq("name", to)
-					.and().eq("module", getId())
-				.query()
-				.get(0);
-		
-		Portal p = new Portal();
-		p.setDestination(loc2);
-		p.setEntry(loc1);
-		p.setName(name);
-		PortalBase.createDao().create(p);
-	}	
-	
 	public static void init(String moduleName) throws SQLException{
 		CaveInitializer.moduleName = moduleName;
 		
 		initWesternCorridor();
+		initEasternRoundabout();
 	}
 	
 	public static void initWesternCorridor() throws SQLException{
 		A1 = prepareLocation("start",
 				"You find yourself in a huge cave. Air here is suffocating and you can barely see anything. " +
 				"It looks like you woke up here.");
-		B1 = prepareLocation("b1","The stone under your feet is very slippery.");
-		B2 = prepareLocation("b2","It looks like the corridor is rising towards the east.");
-		B3 = prepareLocation("b3","In this small cave there is a small campfire in the middle.");
-		C3 = prepareLocation("c3", "It is so dark here, you think you are blind.");
-		D3 = prepareLocation("d3", "This small cave is where three narrow passages meet.");
-		D2 = prepareLocation("d2", "You have to crawl to keep moving. You are afraid you may get stuck.");
+		B1 = prepareLocation("b1",
+				"The stone under your feet is very slippery.");
+		B2 = prepareLocation("b2",
+				"It looks like the corridor is rising towards the east.");
+		B3 = prepareLocation("b3",
+				"In this small cave there is a small campfire in the middle.");
+		C3 = prepareLocation("c3", 
+				"It is so dark here, you think you are blind.");
+		D3 = prepareLocation("d3", 
+				"This small cave is where three narrow passages meet.");
+		D2 = prepareLocation("d2", 
+				"You have to crawl to keep moving. You are afraid you may get stuck.");
 		D1 = prepareLocation("d1", 
 				"The tunnel turns slightly here. It's small and you have to watch out not to hit yourself on the head.");
-		E1 = prepareLocation("e1", "This part of the cave is relatively big. You can hear water dripping.");
+		E1 = prepareLocation("e1", 
+				"This part of the cave is relatively big. You can hear water dripping.");
 		F1 = prepareLocation("f1", 
 				"This is a small room, with a hole in the middle. Oddly enough there is a hole in the middle with" +
 				"a sinister glow coming from within. If you decide to descend, you won't be able to return.");
@@ -84,5 +71,38 @@ public class CaveInitializer {
 		D2.connectTwoWay(D1, "west", "east");
 		D1.connectTwoWay(E1, "south", "north");
 		E1.connectTwoWay(F1, "south", "north");
+	}
+	
+	public static void initEasternRoundabout() throws SQLException{
+		D4 = prepareLocation("d4", 
+				"Narrow passage.");
+		D5 = prepareLocation("d5", 
+				"A cavernal crossroad of some sort. There is a sign, pointing left, but you can't understand what it says.");
+		C5 = prepareLocation("c5", 
+				"Two tiny tunnels meet here. To go further you must squeaze through a crack in the wall.");
+		C6 = prepareLocation("c6", 
+				"The cave is so narrow here you start thinking it just wants to hug you.");
+		C7 = prepareLocation("c7", 
+				"To your suprise you see an apple tree! It seems to be growing out of the rocky ground. It " +
+				"even has leaves. The tree seems to have a faint glow about it...");
+		D7 = prepareLocation("d7", 
+				"The ground here is moist and slippery. You have to go very carefully.");
+		E7 = prepareLocation("e7", 
+				"This is some sort of an underground pond. You are forced to go knee deep in cold and smelly water.");
+		E6 = prepareLocation("e6", 
+				"Instead of the usual rock ground here is made of sticky mud. It smells terribly.");
+		E5 = prepareLocation("e5", 
+				"Tunnel turns gently here. You think you can see mushrooms growing on the wall.");
+		
+		D4.connectTwoWay(D3, "west", "east");
+		D4.connectTwoWay(D5, "east", "west");
+		D5.connectTwoWay(C5, "north", "south");
+		C5.connectTwoWay(C6, "east", "west");
+		C6.connectTwoWay(C7, "east", "west");
+		C7.connectTwoWay(D7, "south", "north");
+		D7.connectTwoWay(E7, "south", "north");
+		E7.connectTwoWay(E6, "west", "east");
+		E6.connectTwoWay(E5, "west", "east");
+		E5.connectTwoWay(D5, "north", "south");
 	}
 }
